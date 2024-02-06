47fdb3db4f141aa09c24a8e00df919e80873ac9a
Josep V. Monjo <jvmonjo@gmail.com>
2024-02-06 13:00:55 +0100

    ADDED: publications feuture

---
+++ src/components/Pubcard.tsx
@@ -0,0 +1,56 @@
+import { slugifyStr } from "@utils/slugify";
+import Datetime from "./Datetime";
+import type { CollectionEntry } from "astro:content";
+
+export interface Props {
+  href?: string;
+  frontmatter: CollectionEntry<"publication">["data"];
+  secHeading?: boolean;
+}
+
+export default function Pubcard({
+  href,
+  frontmatter,
+  secHeading = true,
+}: Props) {
+  const { title, pubDatetime, modDatetime, ogImage, tags } = frontmatter;
+
+  const headerProps = {
+    style: { viewTransitionName: slugifyStr(title) },
+    className: "text-lg font-medium decoration-dashed hover:underline",
+  };
+
+  return (
+    <li className="my-6">
+      <div className="grid grid-cols-4 gap-2">
+        <div className="col-span-1">
+          <img src={`${ogImage}`} alt={title} width={200} height={400} />
+        </div>
+        <div className="col-span-3">
+          <a
+            href={href}
+            className="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
+          >
+            {secHeading ? (
+              <h2 {...headerProps}>{title}</h2>
+            ) : (
+              <h3 {...headerProps}>{title}</h3>
+            )}
+          </a>
+          <Datetime pubDatetime={pubDatetime} modDatetime={modDatetime} />
+          <br />
+          <div className="flex flex-wrap gap-2">
+            {tags.map(tag => (
+              <span
+                key={tag}
+                className="px-2 py-1 text-xs font-medium bg-skin-accent text-skin-base rounded-md"
+              >
+                {tag}
+              </span>
+            ))}
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+}
---
+++ src/content/publication/2013-sociologia.md
@@ -0,0 +1,17 @@
+---
+pubDatetime: 2013-06-01
+modDatetime:
+author: Josep Monjo
+type: tfm
+publisher: Universitat de València
+title: Els discursos com a mecanisme legitimador de polítiques restrictives de llibertat. El delicte d'injúries a la Corona
+description: Premi extraordinari de Màster 2013. L'objectiu del treball és esbrinar quin és el mecanisme mitjançant el qual els discursos serveixen com a eina legitimadora de polítiques criminals restrictives de llibertats, basant-nos en els discursos generats al voltant del delicte d'injúries a la Corona.
+url: "https://github.com/jvmonjo/tfm-sociologia/releases/latest/download/main.pdf"
+ogImage: /assets/publications/2013-tfm-sociologia.png
+tags:
+  - master
+  - sociologia
+  - criminologia
+  - polítiques públiques
+  - anàlisi del discurs
+---
